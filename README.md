509aa69247f67cfdca3021fea98b99ac4ee24792
officenonstop <officenonstop.com@gmail.com>
2025-11-11 19:11:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+Version 1.0
+
+Initial base version
+
+apps.json
+  ERPNext and Modules - check apps.json with versions
+Container file 
+  PYTHON_VERSION=3.11.6
+  NODE_VERSION=20.19.2
+  FRAPPE_BRANCH=version-15
+build.sh
+  FRAPPE_BRANCH=v15.83.0
+  PYTHON_VERSION=3.11.6
+  NODE_VERSION=20.19.2
+my.yml
+  mariadb:10.7
+  redis:6.2-alpine
---
+++ build.sh
@@ -0,0 +1,17 @@
+set -x
+snap install docker
+
+echo "Waiting for 10 sec for docker service to come up ..."
+sleep 10
+
+export APPS_JSON_BASE64=$(base64 -w 0 ./apps.json)
+echo -n ${APPS_JSON_BASE64} | base64 -d 
+
+docker buildx build \
+  --build-arg "FRAPPE_PATH=https://github.com/frappe/frappe" \
+  --build-arg "FRAPPE_BRANCH=v15.83.0" \
+  --build-arg "PYTHON_VERSION=3.11.6" \
+  --build-arg "NODE_VERSION=20.19.2" \
+  --build-arg "APPS_JSON_BASE64=$APPS_JSON_BASE64" \
+  --tag erpnext_generic:v15.0 \
+  --file images/custom/Containerfile .
