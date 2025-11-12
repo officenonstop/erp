7800d678f14ab20d8655988d705d2b551dc8b2a8
officenonstop <officenonstop.com@gmail.com>
2025-11-12 21:16:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,15 @@ sleep 10
 export APPS_JSON_BASE64=$(base64 -w 0 ./apps.json)
 echo -n ${APPS_JSON_BASE64} | base64 -d 
 
+docker builder prune -f
+docker system prune -a
+
 docker buildx build \
   --build-arg "FRAPPE_PATH=https://github.com/frappe/frappe" \
   --build-arg "FRAPPE_BRANCH=v15.83.0" \
   --build-arg "PYTHON_VERSION=3.11.6" \
   --build-arg "NODE_VERSION=20.19.2" \
   --build-arg "APPS_JSON_BASE64=$APPS_JSON_BASE64" \
-  --tag erp:generic_v15.0 \
-  --file images/custom/Containerfile .
+  --tag officenonstop/erp:generic_v15.0 \
+  --file images/custom/Containerfile . \
+  --progress=plain 2>&1 | tee build.log
